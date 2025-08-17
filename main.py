d014981ec2e04982dc6c035fe18ed7b775841dbc
Bruno Saunders <dig.brunosilva@opovo.local>
2025-08-16 23:02:48 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print(
+    "Jornada de Dados"
+)
+
+idade = 29
+nome = 'Bruno'
+
+print (idade)
+
+nova_idade = idade + 1
+
+print(nova_idade)
+
+lista = [1,2,3,4,5]
+
+lista_invertida = lista.reverse()
